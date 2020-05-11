dd028efa6bf78ae3c5b5add1981777208e9dc857
Himali Gunjal <himaligunjal31@gmail.com>
2020-05-11 11:47:52 +0530

    commit

--- src/main/java/com/cg/realestate/ZuulApplication.java
+++ src/main/java/com/cg/realestate/ZuulApplication.java
@@ -20,22 +20,4 @@ public class ZuulApplication {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 
-	 @Bean
-	    @Primary
-	    public CorsFilter corsFilter() {
-	        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        final CorsConfiguration config = new CorsConfiguration();
-	        config.setAllowCredentials(true);
-	        config.addAllowedOrigin("*");
-	        config.addAllowedHeader("*");
-	        config.addAllowedMethod("OPTIONS");
-	        config.addAllowedMethod("HEAD");
-	        config.addAllowedMethod("GET");
-	        config.addAllowedMethod("PUT");
-	        config.addAllowedMethod("POST");
-	        config.addAllowedMethod("DELETE");
-	        config.addAllowedMethod("PATCH");
-	        source.registerCorsConfiguration("/**", config);
-	        return new CorsFilter(source);
-	    }
 }
