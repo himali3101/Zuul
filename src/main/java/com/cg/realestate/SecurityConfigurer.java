f62b1edfb46d30cf8bb636a263638da81bde299a
Himali Gunjal <himaligunjal31@gmail.com>
2020-05-12 09:15:08 +0530

    jwt

---
+++ src/main/java/com/cg/realestate/SecurityConfigurer.java
@@ -0,0 +1,33 @@
+package com.cg.realestate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
+
+
+	@Autowired
+	JwtRequestFilter jwtRequestFilter;
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/login/authenticate").hasAuthority("ADMIN")
+		.antMatchers("/login/authenticate").permitAll()
+		.antMatchers("/login/signup").hasAuthority("ADMIN")
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
